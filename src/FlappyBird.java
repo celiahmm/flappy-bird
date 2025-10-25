b2062f6a8342ca02670f779227fdf05fc6a05e23
celia h. <celiahamam@gmail.com>
2025-10-25 22:37:17 +0200

    update

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -1,18 +1,113 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class FlappyBird extends JFrame {
-	JFrame frame;
-	JLabel background = new JLabel(new ImageIcon("src/images/bg.png"));
+public class FlappyBird extends JPanel implements ActionListener, KeyListener {
+
+	int boardWidth = 360;
+	int boardHeight = 640;
+
+
+	// images
+	Image backgroundImg;
+	Image birdImg;
+	Image topPipeImg;
+	Image bottomPipeImg;
+
+	// bird
+	int birdX = boardWidth / 8;
+	int birdY = boardHeight / 2;
+	int birdWidth = 34;
+	int birdHeight = 24;
+
+	class Bird {
+		int x = birdX;
+		int y = birdY;
+		int width = birdWidth;
+		int height= birdHeight;
+		Image img;
+
+		Bird(Image img) {
+			this.img = img;
+		}
+	}
+
+	// pipes
+	int pipeX = boardWidth;
+	int pipeY = 0;
+	int pipeWidth = 64; // scaled by 1/6
+	int pipeHeight = 512;
+
+
+
+	// game logic
+	Bird bird;
+	int velocityY = 0;
+	int gravity = 1;
+
+	Timer gameLoop;
 
 	FlappyBird() {
-		frame = new JFrame("Flappy Bird");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(360,640);
-		frame.setResizable(false);
+		setPreferredSize(new Dimension(boardWidth, boardHeight));
+		setFocusable(true);
+		addKeyListener(this);
 
-		frame.setContentPane(background);
-		frame.setVisible(true);
+		// load images
+		backgroundImg = new ImageIcon(getClass().getResource("./bg.png")).getImage();
+		birdImg = new ImageIcon(getClass().getResource("bird.png")).getImage();
+		topPipeImg = new ImageIcon(getClass().getResource("toppipe.png")).getImage();
+		bottomPipeImg = new ImageIcon(getClass().getResource("bottompipe.png")).getImage();
+
+		// bird
+		bird = new Bird(birdImg);
+
+		// game timer
+		gameLoop = new Timer(1000/60, this); // 60fps
+		gameLoop.start();
 
 	}
 
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		draw(g);
+	}
+
+	public void draw(Graphics g) {
+		// background
+		g.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight, null);
+
+		//bird
+		g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
+	}
+
+	public void move() {
+		// bird
+		velocityY += gravity;
+		bird.y += velocityY;
+		bird.y = Math.max(bird.y, 0);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		move();
+		repaint(); // call paintComponent()
+	}
+
+
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+			velocityY = -9 ;
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {}
+	@Override
+	public void keyReleased(KeyEvent e) {}
 }
--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,19 @@
+import javax.swing.*;
+
 public class Game {
     public static void main(String[] args) {
 
+	    JFrame frame = new JFrame("Flappy Bird");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(360,640);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+
 		FlappyBird flappyBird = new FlappyBird();
+		frame.add(flappyBird);
+		frame.pack();
+		flappyBird.requestFocus();
+
+	    frame.setVisible(true);
     }
 }
